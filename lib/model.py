ba7ccdb8572b80804a8929dc0e609fc24673da82
Jordan Ayala <jmayala@protonmail.com>
2021-05-06 12:41:28 -0400

    Train model

--- lib/model.py
+++ lib/model.py
@@ -50,17 +50,22 @@ class MLParams:
 
 
 class TrainingParams(MLParams):
-    def __init__(self, tl_learning_rate: float, fine_learning_rate: float, tl_epochs: int, fine_epochs: int):
+    def __init__(self,
+                 tl_learning_rate: float, fine_learning_rate: float,
+                 tl_epochs: int, fine_epochs: int,
+                 fine_layers: int):
         self.tl_learning_rate = tl_learning_rate
         self.fine_learning_rate = fine_learning_rate
         self.tl_epochs = tl_epochs
         self.fine_epochs = fine_epochs
+        self.fine_layers = fine_layers
 
     def as_name(self):
         return f"tlr{self.tl_learning_rate}-" \
                f"flr{self.fine_learning_rate}-" \
                f"tep{self.tl_epochs}-" \
-               f"fep{self.fine_epochs}"
+               f"fep{self.fine_epochs}-" \
+               f"{self.fine_layers}"
 
 
 class ModelParams(MLParams):
@@ -69,12 +74,10 @@ class ModelParams(MLParams):
                  image_size: int,
                  num_classes: int,
                  dropout: float,
-                 train_layers: int,
                  global_pooling: GlobalPooling,
                  use_data_augmentation: bool):
         self.base_model = base_model
         self.num_classes = num_classes
-        self.train_layers = train_layers
         self.dropout = dropout
         self.image_size = image_size
         self.global_pooling = global_pooling
@@ -83,7 +86,6 @@ class ModelParams(MLParams):
     def as_name(self):
         return f"bm{self.base_model.name}-" \
                f"nc{self.num_classes}-" \
-               f"tl{self.train_layers}-" \
                f"dr{self.dropout}-" \
                f"im{self.image_size}-" \
                f"gp{self.global_pooling.name}-" \
@@ -96,7 +98,6 @@ class ModelParams(MLParams):
             base_model=BaseModel[d['base_model']],
             num_classes=d['num_classes'],
             dropout=d['dropout'],
-            train_layers=d['train_layers'],
             image_size=d['image_size'],
             global_pooling=GlobalPooling[d['global_pooling']],
             use_data_augmentation=d['use_data_augmentation']
@@ -141,10 +142,105 @@ def make_model(model_params: ModelParams):
     outputs = tf.keras.layers.Dense(units)(x)
     model = tf.keras.Model(inputs, outputs, name=model_params.as_name())
 
-    return model
+    return model, base_model.name
 
 
 class RetinopathyModel:
     def __init__(self, model_params: ModelParams):
-        self.model_params = model_params
-        self.model = make_model(model_params)
+        self.params = model_params
+        model, base_model_name = make_model(model_params)
+        self.model: tf.keras.Model = model
+        self.base_model_name: str = base_model_name
+
+
+def transfer_learn(retinopathy: RetinopathyModel,
+                   train_dataset,
+                   validation_dataset,
+                   training_params: TrainingParams,
+                   callbacks: tf.keras.callbacks.Callback = None,
+                   verbose=0):
+    """
+    Train model using transfer learning
+    :param retinopathy: a retinopathy model
+    :param train_dataset: training dataset
+    :param validation_dataset: validation dataset
+    :param training_params: training parameters
+    :param callbacks: optional list of callbacks
+    :param verbose: verbose logging level
+    :return: (training history, fitted model)
+    """
+    if callbacks is None:
+        callbacks = []
+
+    loss_fun = tf.keras.losses.BinaryCrossentropy(from_logits=True) if retinopathy.params.num_classes <= 2 \
+        else tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+
+    model = retinopathy.model
+
+    model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=training_params.tl_learning_rate),
+                  loss=loss_fun,
+                  metrics=['accuracy'])
+
+    history = model.fit(train_dataset,
+                        epochs=training_params.tl_epochs,
+                        validation_data=validation_dataset,
+                        callbacks=callbacks,
+                        verbose=verbose)
+
+    return history, model
+
+
+def fine_tune(retinopathy: RetinopathyModel,
+              train_dataset,
+              validation_dataset,
+              training_params: TrainingParams,
+              callbacks: tf.keras.callbacks.Callback = None,
+              verbose=0):
+    """
+    Train model using fine tuning
+    :param retinopathy: a retinopathy model (must have been trained with transfer_learn)
+    :param train_dataset: training dataset
+    :param validation_dataset: validation dataset
+    :param training_params: training parameters
+    :param callbacks: optional list of callbacks
+    :param verbose: verbose logging level
+    :return: (training history, fitted model)
+    """
+
+    if callbacks is None:
+        callbacks = []
+
+    model = retinopathy.model
+    base_model_name = retinopathy.base_model_name
+    num_classes = retinopathy.params.num_classes
+
+    base_model = model.get_layer(base_model_name)
+    base_model.trainable = True
+
+    # Fine-tune n layers from the top
+    fine_layers = training_params.fine_layers
+    n_layers = len(base_model.layers)
+    fine_tune_at = max(n_layers - fine_layers, 0)
+
+    # Freeze all the layers before the `fine_tune_at` layer
+    for layer in base_model.layers[:fine_tune_at]:
+        layer.trainable = False
+
+    loss_fun = tf.keras.losses.BinaryCrossentropy(from_logits=True) if num_classes <= 2 \
+        else tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+
+    model.compile(loss=loss_fun,
+                  optimizer=tf.keras.optimizers.RMSprop(learning_rate=training_params.fine_learning_rate),
+                  metrics=['accuracy'])
+
+    total_epochs = training_params.tl_epochs + training_params.fine_epochs
+
+    history = model.fit(train_dataset,
+                        epochs=total_epochs,
+                        initial_epoch=training_params.tl_epochs,
+                        validation_data=validation_dataset,
+                        callbacks=callbacks,
+                        verbose=verbose
+                        )
+
+    return history, model
--- main_evo.py
+++ main_evo.py
@@ -1,4 +1,33 @@
-from lib import model
+from lib import model, dataset
+import tensorflow as tf
+
+BATCH_SIZE = 32
+SPLITS = [0.65, 0.25, 0.15]
+SPLITS_NAMES = ["Train", "Validation", "Test"]
+PROJECT_ROOT = '.'
+
+
+def dataset_defaults(
+        img_size: int,
+        splits=SPLITS,
+        remap_with=dataset.RetinopathyV2.mappings['c2'],
+        shuffle_batches=True,
+        reshuffle=False):
+    tf.random.set_seed(42)
+    img_size = (img_size, img_size)
+
+    return dataset.build_dataset(
+        dataset.RetinopathyV2,
+        PROJECT_ROOT,
+        img_size,
+        BATCH_SIZE,
+        splits,
+        remap_classes=remap_with,
+        shuffle_split=True,
+        shuffle_batches=shuffle_batches,
+        reshuffle=reshuffle
+    )
+
 
 if __name__ == '__main__':
     model_params = model.ModelParams(
@@ -6,7 +35,38 @@ if __name__ == '__main__':
         image_size=160,
         num_classes=2,
         dropout=0.2,
-        train_layers=2,
         global_pooling=model.GlobalPooling.AVG_POOLING,
         use_data_augmentation=True
     )
+
+    training_params = model.TrainingParams(
+        tl_learning_rate=0.0001,
+        tl_epochs=10,
+        fine_learning_rate=0.00001,
+        fine_epochs=5,
+        fine_layers=5
+    )
+
+    print(model_params)
+    print(training_params)
+
+    retinopathy_resnet50 = model.RetinopathyModel(model_params)
+
+    datasets, class_names, images_df = dataset_defaults(img_size=model_params.image_size)
+    train_dataset, validation_dataset, test_dataset = datasets
+
+    tl_history, tl_model = model.transfer_learn(retinopathy_resnet50,
+                                                train_dataset,
+                                                validation_dataset,
+                                                training_params,
+                                                verbose=0)
+
+    tl_loss, tl_accuracy = tl_model.evaluate(test_dataset)
+
+    fine_history, fine_model = model.fine_tune(retinopathy_resnet50,
+                                               train_dataset,
+                                               validation_dataset,
+                                               training_params,
+                                               verbose=1)
+
+    fine_loss, fine_accuracy = fine_model.evaluate(test_dataset)
